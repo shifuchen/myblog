fd8a7d076c2e8ebbddffd171e2a1d7ff3fee375d
DELL <1621444214@qq.com>
2019-11-08 13:42:36 +0800

    前台展示

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,12 +2,17 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Db;
+use think\Log;
 
 class Index extends Controller
 {
     public function index()
     {
-        return $this->fetch("index");
+        $data=Db::name("content")->paginate(10);
+        $page = $data->render();
+        Log::error($page);
+        return $this->fetch("index",['list'=>$data,'page'=>$page]);
     }
 
     public function details(){
