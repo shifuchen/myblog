b819ad85973d5614464e96db50549fe7fc20f8f3
DELL <1621444214@qq.com>
2019-11-09 20:59:28 +0800

    前台展示

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -18,9 +18,12 @@ class Index extends Controller
 
     public function details()
     {
-        $contentDetail = Db::name("content")->where('id', input('id'))->find();
-        Log::error(json($contentDetail));
-        return $this->fetch("details", ['list' => $contentDetail]);
+        $contentDetail   = Db::name("content")->where('id', input('id'))->find();
+        $field           = ['commentator', 'commentator_id', 'commcontent', 'content_id', 'FROM_UNIXTIME(createtime,"%Y-%m-%d") as createtime'];
+        $contentCommlist = Db::name("comment")->where('content_id', input('id'))->field($field)->select();
+        $contentCommlist['count'] = Db::name("comment")->where('content_id', input('id'))->field($field)->count();
+            Log::error($contentCommlist);
+        return $this->fetch("details", ['list' => $contentDetail, 'comments' => $contentCommlist]);
     }
 
     public function addComment(Request $request)
@@ -29,21 +32,21 @@ class Index extends Controller
         $userSession = Session::get("indexUser");
         $result      = null;
         $count       = 0;
-        if (empty($userSession)) {
-            $result['code'] = 100021;
-            $result['msg']='登录后才能评论,请先登录!';
+//        if (empty($userSession)) {
+//            $result['code'] = 100021;
+//            $result['msg']='登录后才能评论,请先登录!';
+//        } else {
+        $count = Db::name("comment")->insert(['commentator'    => $userSession['username'],
+                                              'commentator_id' => $userSession['id'], 'commcontent' => $data['desc'],
+                                              'content_id'     => $data['content_id'], 'createtime' => time(), 'updatetime' => time()]);
+        if ($count > 0) {
+            $result['code'] = 0;
+            $result['msg']  = '添加成功';
         } else {
-            $count = Db::name("comment")->insert(['commentator'    => $userSession['username'],
-                                                  'commentator_id' => $userSession['id'], 'commcontent' => $data['desc'],
-                                                  'content_id'     => $data['content_id'], 'createtime' => time(), 'updatetime' => time()]);
-            if($count>0){
-                $result['code'] = 0;
-                $result['msg']='添加成功';
-            }else{
-                $result['code'] = 100020;
-                $result['msg']='添加评论失败!';
-            }
+            $result['code'] = 100020;
+            $result['msg']  = '添加评论失败!';
         }
+//        }
         return json($result);
     }
 }
